db52957de5a9ef8e62bdf13ac4ea253802d0ac7e
Greg Sarjeant <gsarjeant@puppetlabs.com>
2016-05-20 14:59:22 -0400

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,26 @@
+class puppet4_template_examples{
+  $local_variable = 'This is the value of the local variable.'
+
+  File {
+    owner => root,
+    group => root,
+    mode  => '0400',
+  }
+
+  # ERB Template example
+  File { '/tmp/erb_template.txt':
+    content => template('puppet4_template_examples/erb_template.erb'),
+  }  
+
+  # EPP Template Example - pull param from local scope
+  File { '/tmp/epp_template_no_param.txt':
+    content => template('puppet4_template_examples/epp_template_no_param.erb'),
+  }  
+
+  # EPP Template Example - pass param
+  File { '/tmp/erb_template.txt':
+    content => template('puppet4_template_examples/erb_template.erb', {
+                 'local_variable' => $local_variable
+               } ),
+  }  
+}
